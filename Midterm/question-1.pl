a76c9738d009897a86a1617abe03b1c7811e4c9e
Ade Naufal Ammar <ade.naufal@gmail.com>
2019-03-11 14:50:11 +0700

    Added some files

---
+++ Midterm/question-1.pl
@@ -0,0 +1,88 @@
+male(farid).
+male(toha).
+male(ade).
+male(didin).
+male(yusuf).
+male(naufal).
+male(ardi).
+male(mbahkakung).
+male(briyan).
+male(ilham).
+
+female(nur).
+female(camelia).
+female(tifarie).
+female(zainab).
+female(sarah).
+female(rista).
+female(mbahputri).
+female(sartini).
+
+parent(mbahkakung,farid).
+parent(mbahkakung,nur).
+parent(mbahkakung,toha).
+parent(mbahkakung,sartini).
+parent(mbahputri,farid).
+parent(mbahputri,nur).
+parent(mbahputri,toha).
+parent(mbahputri,sartini).
+parent(farid,ade).
+parent(farid,camelia).
+parent(farid,tifarie).
+parent(nur,zainab).
+parent(nur,didin).
+parent(nur,sarah).
+parent(nur,yusuf).
+parent(toha,naufal).
+parent(toha,rista).
+parent(toha,ardi).
+parent(sartini,briyan).
+parent(sartini,ilham).
+
+birth(farid,1965).
+birth(nur,1969).
+birth(toha,1950).
+birth(sartini,1955).
+
+sibling(X,Y):- parent(Z,X),parent(Z,Y).
+grandparent(X,Y):- parent(X,Z),parent(Z,Y).
+
+/* Bulik in Javanese family tree, is known as younger aunt. If your parent has younger sister in their family tree, then you can call them as 'bulik'. */
+
+bulik(Y,X):-
+	female(Y),
+	sibling(Y,P),
+	parent(P,X),
+	birth(Y,A),
+	birth(P,B),
+	A>B.
+	
+/* Pakde in Javanese family tree, is known as older uncle. If your parent has older brother in their family tree, then you can call them as 'pakde'. */
+
+pakde(Y,X):-
+	male(Y),
+	sibling(Y,P),
+	parent(P,X),
+	birth(Y,A),
+	birth(P,B),
+	A<B.
+
+/* Paklik in Javanese family tree, is known as younger uncle. If your parent has younger brother in their family tree, then you can call them as 'paklik'. */	
+
+paklik(Y,X):-
+	male(Y),
+	sibling(Y,P),
+	parent(P,X),
+	birth(Y,A),
+	birth(P,B),
+	A>B.
+
+/* Bude in Javanese family tree, is known as older aunt. If your parent has older sister in their family tree, then you can call them as 'bude'. */
+	
+bude(Y,X):-
+	female(Y),
+	sibling(Y,P),
+	parent(P,X),
+	birth(Y,A),
+	birth(P,B),
+	A<B.
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-prolog-hw
+# prolog-hw
+
+This Git repo contains my codes in Prolog.
+
+Copyright © 2019 Ade Naufal Ammar ade@nitipmerch.com
+
+This work is free. You can redistribute it and/or modify it under the
+terms of the Do What The Fuck You Want To Public License, Version 2,
+as published by Sam Hocevar. See the COPYING file for more details.
+
+<a href="http://www.wtfpl.net/"><img src="http://www.wtfpl.net/wp-content/uploads/2012/12/wtfpl-badge-4.png" width="80" height="15" alt="WTFPL" /></a>
