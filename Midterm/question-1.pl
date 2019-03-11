9650f0a6ef23cfb50ca5656ba1b9bb5696f5f4c9
Ade Naufal Ammar <ade.naufal@gmail.com>
2019-03-12 00:03:07 +0700

    Tidying up the code

--- Midterm/question-1.pl
+++ Midterm/question-1.pl
@@ -53,11 +53,12 @@ married(surtinah,toha).
 married(eka,farid).
 married(suhaimin,sartini).
 
-spouse(X,Y):- parent(X,Z),parent(Y,Z).
 sibling(X,Y):- parent(Z,X),parent(Z,Y).
 grandparent(X,Y):- parent(X,Z),parent(Z,Y).
 
-/* Bulik in Javanese family tree, is known as younger aunt. If your parent has younger sister in their family tree, then you can call them as 'bulik'. */
+/* Bulik in Javanese family tree, is known as younger aunt.
+If your parent has younger sister in their family tree,
+then you can call them as 'bulik'. */
 
 bulik(Y,X):- % maternal
 	female(Y),
@@ -76,7 +77,9 @@ bulik(Y,X):- % marriage
 	birth(P,B),
 	A>B.
 	
-/* Pakde in Javanese family tree, is known as older uncle. If your parent has older brother in their family tree, then you can call them as 'pakde'. */
+/* Pakde in Javanese family tree, is known as older uncle.
+If your parent has older brother in their family tree,
+then you can call them as 'pakde'. */
 
 pakde(Y,X):- % maternal
 	male(Y),
@@ -95,7 +98,9 @@ pakde(Y,X):- % marriage
 	birth(P,B),
 	A<B.
 
-/* Paklik in Javanese family tree, is known as younger uncle. If your parent has younger brother in their family tree, then you can call them as 'paklik'. */	
+/* Paklik in Javanese family tree, is known as younger uncle.
+If your parent has younger brother in their family tree,
+then you can call them as 'paklik'. */	
 
 paklik(Y,X):- % maternal
 	male(Y),
@@ -114,7 +119,9 @@ paklik(Y,X):- % marriage
 	birth(P,B),
 	A>B.
 
-/* Bude in Javanese family tree, is known as older aunt. If your parent has older sister in their family tree, then you can call them as 'bude'. */
+/* Bude in Javanese family tree, is known as older aunt.
+If your parent has older sister in their family tree,
+then you can call them as 'bude'. */
 	
 bude(Y,X):- % maternal
 	female(Y),
