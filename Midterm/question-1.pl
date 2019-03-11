f4918e72421b31d303aca045ce34c455f6b9ba5c
Ade Naufal Ammar <ade.naufal@gmail.com>
2019-03-11 22:49:15 +0700

    Add marriage rules

--- Midterm/question-1.pl
+++ Midterm/question-1.pl
@@ -8,7 +8,11 @@ male(ardi).
 male(mbahkakung).
 male(briyan).
 male(ilham).
+male(kahir).
+male(suhaimin).
 
+female(eka).
+female(surtinah).
 female(nur).
 female(camelia).
 female(tifarie).
@@ -44,22 +48,37 @@ birth(nur,1969).
 birth(toha,1950).
 birth(sartini,1955).
 
+married(kahir,nur).
+married(surtinah,toha).
+married(eka,farid).
+married(suhaimin,sartini).
+
+spouse(X,Y):- parent(X,Z),parent(Y,Z).
 sibling(X,Y):- parent(Z,X),parent(Z,Y).
 grandparent(X,Y):- parent(X,Z),parent(Z,Y).
 
 /* Bulik in Javanese family tree, is known as younger aunt. If your parent has younger sister in their family tree, then you can call them as 'bulik'. */
 
-bulik(Y,X):-
+bulik(Y,X):- % maternal
 	female(Y),
 	sibling(Y,P),
 	parent(P,X),
 	birth(Y,A),
 	birth(P,B),
 	A>B.
+
+bulik(Y,X):- % marriage
+	female(Y),
+	married(Y,Z),
+	sibling(Z,P),
+	parent(P,X),
+	birth(Z,A),
+	birth(P,B),
+	A>B.
 	
 /* Pakde in Javanese family tree, is known as older uncle. If your parent has older brother in their family tree, then you can call them as 'pakde'. */
 
-pakde(Y,X):-
+pakde(Y,X):- % maternal
 	male(Y),
 	sibling(Y,P),
 	parent(P,X),
@@ -67,9 +86,18 @@ pakde(Y,X):-
 	birth(P,B),
 	A<B.
 
+pakde(Y,X):- % marriage
+	male(Y),
+	married(Y,Z),
+	sibling(Z,P),
+	parent(P,X),
+	birth(Z,A),
+	birth(P,B),
+	A<B.
+
 /* Paklik in Javanese family tree, is known as younger uncle. If your parent has younger brother in their family tree, then you can call them as 'paklik'. */	
 
-paklik(Y,X):-
+paklik(Y,X):- % maternal
 	male(Y),
 	sibling(Y,P),
 	parent(P,X),
@@ -77,12 +105,30 @@ paklik(Y,X):-
 	birth(P,B),
 	A>B.
 
+paklik(Y,X):- % marriage
+	male(Y),
+	married(Y,Z),
+	sibling(Z,P),
+	parent(P,X),
+	birth(Z,A),
+	birth(P,B),
+	A>B.
+
 /* Bude in Javanese family tree, is known as older aunt. If your parent has older sister in their family tree, then you can call them as 'bude'. */
 	
-bude(Y,X):-
+bude(Y,X):- % maternal
 	female(Y),
 	sibling(Y,P),
 	parent(P,X),
 	birth(Y,A),
+	birth(P,B),
+	A<B.
+	
+bude(Y,X):- % marriage
+	female(Y),
+	married(Y,Z),
+	sibling(Z,P),
+	parent(P,X),
+	birth(Z,A),
 	birth(P,B),
 	A<B.
